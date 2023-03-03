1d514288e8941b383c7ac1bb06148746c746f622
Aarsh <aarshbatra.in@gmail.com>
2023-03-03 15:17:35 +0530

    created a new folder at the root called appPublic to store the public version of the app. Removed the app.R file from the root. Using this new directory also helps while publishing as it reduces the likelihood that we will run into the 'max package size' reached error as having a separate folder helps us keep track of what files we are pushing to shinyapps.io.

--- appPublic/app.R
+++ appPublic/app.R
@@ -1,5 +1,5 @@
 # setting global options
-options(shiny.maxRequestSize = 900*1024^2)
+options(shiny.maxRequestSize = 1200*1024^2)
 
 # download libraries
 library(shiny)
@@ -11,13 +11,15 @@ library(shinythemes)
 library(shinycssloaders)
 library(shinyjs)
 library(bslib)
+library(viridis)
+
 
 # source data
-source("./R/aqli.data.explorer.helper.script.R")
+# source("aqli.data.explorer.helper.script.R")
 
 # loading the .RData file that contain all required data objects
 # save(list = ls(all = TRUE), file= "all.RData")
-# load("all.RData", .GlobalEnv)
+load("all.RData", .GlobalEnv)
 
 # Define UI
 ui <- fluidPage(
@@ -29,7 +31,6 @@ ui <- fluidPage(
       # column(width = 10, h2("AQLI Data Explorer", style = "margin-top: -5px;"), offset = 1)
     )
   ),
-  shiny::tags$br(),
   shiny::tags$br(),
   tabsetPanel(
     tabPanel("Raw Data Exploration",
@@ -95,6 +96,7 @@ ui <- fluidPage(
     ),
     tabPanel("Pollution/LYL Maps (coming soon!)",
              shiny::tags$br(),
+             shiny::tags$br(),
              fluidRow(
                column(3, selectizeInput("country4", "Country", choices = unique(gadm2_aqli_2021$country))),
                column(3, selectInput("graph_type", "Type", choices = c("Pollution", "LYL"), selected = "Pollution")),
@@ -105,8 +107,48 @@ ui <- fluidPage(
              # plotly::plotlyOutput("map"),
              shinycssloaders::withSpinner(plotOutput("map"))
     ),
+    tabPanel("Distributions",
+             shiny::tags$br(),
+             fluidRow(
+               column(2, selectizeInput("continent5", "Continent", choices = c("World", unique(gadm2_aqli_2021$continent)))),
+               column(2, selectizeInput("country5", "Country", choices = NULL)),
+               column(2, selectizeInput("state5", "State", choices = NULL)),
+               column(2, selectizeInput("district5", "District", choices = NULL)),
+               column(3, selectInput("dist_type5", "Distribution Type", choices = c("PM2.5 Pollution", "LYL rel to WHO standard",
+                                                                                    "LYL rel to National standard"))),
+               column(1, selectInput("year5", "Year", choices = c(latest_year:first_year)))
+
+             ),
+             shiny::tags$br(),
+             shiny::tags$hr(),
+             plotOutput("distribution_plot_5")
+             ),
     tabPanel("Compare Regions (coming soon!)"),
-    tabPanel("About AQLI (coming soon!)")
+    tabPanel("About AQLI (WIP!)",
+             shiny::tags$br(),
+             shiny::tags$h6("The Air Quality Life Index, or AQLI, converts air pollution concentrations into their impact on life expectancy. From this, the public and policymakers alike can determine the benefits of air pollution policies in
+             perhaps the most important measure that exists: longer lives."),
+             shiny::tags$br(),
+             shiny::tags$p("The AQLI is rooted in peer-reviewed research by an international team of scholars, including Michael Greenstone from the University of Chicago, which for the first time quantified the causal relationship between long-term human exposure to air pollution and life expectancy. The Index combines this research with hyper-localized, global particulate matter measurements. This unique approach makes the AQLI the first pollution index to show what the threat of air pollution means to a person’s life anywhere in the world. It can also illustrate the gains in life expectancy that could be achieved by reducing particulate pollution concentrations to meet the World Health
+                            Organization (WHO) guidelines, existing national air quality standards, or user-defined levels."),
+             shiny::tags$br(),
+             shiny::tags$p("The video below touches on some key aspects of the AQLI interactive pollution and life years lost map tool:"),
+             shiny::tags$br(),
+             shiny::tags$div(
+               style = "text-align:center;",
+               # tags$iframe(width = "100%", height = "315",
+               #             src = "https://www.youtube.com/watch?v=_z-_D9vxCWE"),
+               HTML('<iframe width="560" height="315" src="aqli_screen_capture_video.mp4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'),
+               shiny::tags$br()
+             ),
+             shiny::tags$br(),
+             "To explore the AQLI interactive tool yourself, visit the AQLI website ", shiny::tags$a("here.", href = "https://aqli.epic.uchicago.edu/the-index/"), "If you are curious
+             to learn more about the methodology underlying the tool, click ", shiny::tags$a("here.", href = "https://aqli.epic.uchicago.edu/about/methodology/"),
+             shiny::tags$br(),
+             shiny::tags$br(),
+             shiny::tags$br()
+
+    )
   )
 )
 
@@ -441,35 +483,131 @@ output$map <- shiny::renderPlot(ggplot() + theme(plot.background = element_rect(
 
   observeEvent(input$generateMap, {
 
-    filteredData3 <- gadm2_aqli_2021_shp %>%
-      dplyr::filter(country == input$country4)
+    # filteredData3 <- gadm2_aqli_2021_shp %>%
+    #   dplyr::filter(country == input$country4)
+    #
+    # if (input$graph_type == "Pollution") {
+    #   map <- filteredData3 %>%
+    #     ggplot2::ggplot() +
+    #     ggplot2::geom_sf(mapping = aes(fill = pm2021)) +
+    #     ggthemes::theme_tufte() +
+    #     viridis::scale_fill_viridis(option = "blues", direction = -1) +
+    #     ggplot2::theme(axis.text = ggplot2::element_blank(),
+    #                    axis.ticks = ggplot2::element_blank()) +
+    #     ggplot2::ggtitle(stringr::str_c("PM2.5 pollution in ", input$country4, " in ", "2021"))
+    #
+    # } else {
+    #   map <- filteredData3 %>%
+    #     ggplot2::ggplot() +
+    #     ggplot2::geom_sf(mapping = aes(fill = llw2021)) +
+    #     ggthemes::theme_tufte() +
+    #     viridis::scale_fill_viridis(option = "rocket", direction = -1) +
+    #     ggplot2::theme(axis.text = ggplot2::element_blank(),
+    #                    axis.ticks = ggplot2::element_blank()) +
+    #     ggplot2::ggtitle(stringr::str_c("Life Years Lost in ", input$country4, " in ", "2021"))
+    #
+    #
+    # }
+    #
+    # # output$map <- plotly::renderPlotly(plotly::ggplotly(map))
+    # output$map <- shiny::renderPlot(map)
 
-    if (input$graph_type == "Pollution") {
-      map <- filteredData3 %>%
-        ggplot2::ggplot() +
-        ggplot2::geom_sf(mapping = aes(fill = pm2021)) +
-        ggthemes::theme_tufte() +
-        viridis::scale_fill_viridis(option = "blues", direction = -1) +
-        ggplot2::theme(axis.text = ggplot2::element_blank(),
-                       axis.ticks = ggplot2::element_blank()) +
-        ggplot2::ggtitle(stringr::str_c("PM2.5 pollution in ", input$country4, " in ", "2021"))
+  })
 
+
+  # Distributions tab--------------------------------------------------------------------
+
+  # continent drop down observer for the distributions tab
+  shiny::observeEvent(input$continent5, {
+    shiny::updateSelectizeInput(session, "country5", choices = c("all", gadm2_aqli_2021 %>% filter(continent == input$continent5) %>% pull(country) %>% unique() ))
+
+  })
+
+  # country drop down observer for the distributions tab
+  shiny::observeEvent(input$country5, {
+    shiny::updateSelectizeInput(session, "state5", choices = c("all", gadm2_aqli_2021 %>% filter(country == input$country5) %>% pull(name_1) %>% unique()))
+
+  })
+
+  # state drop down observer for the distributions tab
+  shiny::observeEvent(input$state5, {
+    shiny::updateSelectizeInput(session, "district5", choices = c("all", gadm2_aqli_2021 %>% filter(name_1 == input$state5) %>% pull(name_2) %>% unique() ))
+
+  })
+
+
+  # filtered data, given the dropdowns for the distributions plot
+  filteredData5 <- shiny::reactive({
+    if(input$continent5 == "World"){
+      gadm2_aqli_2021
     } else {
-      map <- filteredData3 %>%
-        ggplot2::ggplot() +
-        ggplot2::geom_sf(mapping = aes(fill = llw2021)) +
-        ggthemes::theme_tufte() +
-        viridis::scale_fill_viridis(option = "rocket", direction = -1) +
-        ggplot2::theme(axis.text = ggplot2::element_blank(),
-                       axis.ticks = ggplot2::element_blank()) +
-        ggplot2::ggtitle(stringr::str_c("Life Years Lost in ", input$country4, " in ", "2021"))
+      if(input$country5 == "all"){
+        gadm2_aqli_2021 %>%
+          dplyr::filter(continent == input$continent5)
+      } else{
+        if(input$state5 == "all"){
+          gadm2_aqli_2021 %>%
+            dplyr::filter(continent == input$continent5, country == input$country5)
+        } else{
+          if(input$district5 == "all"){
+            gadm2_aqli_2021 %>%
+              dplyr::filter(continent == input$continent5, country == input$country5, name_1 == input$state5)
+          } else {
+            gadm2_aqli_2021 %>%
+              dplyr::filter(continent == input$continent5, country == input$country5, name_1 == input$state5, name_2 == input$district5)
+          }
 
-
+        }
+      }
     }
 
-    # output$map <- plotly::renderPlotly(plotly::ggplotly(map))
-    output$map <- shiny::renderPlot(map)
   })
+
+# plot the distribution given the selected options
+
+output$distribution_plot_5 <- shiny::renderPlot({
+
+  # reactive pollution column name
+  pol_col_5 <- shiny::reactive({
+    stringr::str_c("pm", input$year5)
+  })
+
+  # reactive llpp_who_col name
+  llpp_who_col_5 <- shiny::reactive({
+    stringr::str_c("llpp_who_", input$year5)
+  })
+
+  # reactive llpp_nat_col name
+  llpp_nat_col_5 <- shiny::reactive({
+    stringr::str_c("llpp_nat_", input$year5)
+  })
+
+  if(input$dist_type5 == "PM2.5 Pollution"){
+    filteredData5() %>%
+      ggplot2::ggplot() +
+      ggplot2::geom_histogram(mapping = aes(x = !!as.symbol(pol_col_5())), color = "white", fill = "cornflowerblue") +
+      ggthemes::theme_hc()
+
+
+  } else if (input$dist_type5 == "LYL rel to WHO standard") {
+    filteredData5() %>%
+      ggplot2::ggplot() +
+      ggplot2::geom_histogram(mapping = aes(x = !!as.symbol(llpp_who_col_5())), color = "white", fill = "darkred") +
+      ggthemes::theme_hc()
+
+
+  } else if (input$dist_type5 == "LYL rel to National standard"){
+    filteredData5() %>%
+      ggplot2::ggplot() +
+      ggplot2::geom_histogram(mapping = aes(x = !!as.symbol(llpp_nat_col_5())), color = "white", fill = "darkred") +
+      ggthemes::theme_hc()
+  }
+
+})
+
+
+
+
 
 }
 
