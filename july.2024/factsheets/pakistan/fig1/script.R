e11d68c79f551b5712d8c9dac6b1a1b78724c9e6
Nishka Sharma <71686549+nishkasharma@users.noreply.github.com>
2024-03-12 15:46:13 -0500

    Pakistan factsheet code, figures and table

---
+++ july.2024/factsheets/pakistan/fig1/script.R
@@ -0,0 +1,51 @@
+# read in the helper file
+source("R/july.2024.helper.script.R")
+
+
+# read and filter AQLI data
+pak_aqli_2022 <- gadm2_aqli_2022 %>%
+  filter(country == "Pakistan") %>%
+  mutate(region = case_when(
+    name_1 == "Islamabad" ~ "Islamabad",
+    name_1 == "Punjab" ~ "Punjab",
+    name_1 != "Islamabad"| name_1 != "Punjab" ~ "All other regions (excluding Islamabad and Punjab)"))
+
+# pakistan fs fig 1 data
+pak_fs_fig1_dataset <- pak_aqli_2022 %>%
+  left_join(gadm2_aqli_2022_shp, by = c("objectid_gadm2" = "obidgadm2")) %>%
+  select('country', 'name_1', 'name_2', 'population', 'pm2022', 'llpp_who_2022', 'geometry') %>%
+  add_aqli_color_scale_buckets("lyl", "llpp_who_2022") %>%
+  select(-geometry, geometry) %>%
+  st_as_sf()
+
+# pakistan fs figure 1
+pak_fs_fig1 <- pak_fs_fig1_dataset %>%
+  ggplot() +
+  geom_sf(mapping = aes(fill = reorder(lyl_bucket, order_lyl_bucket)), color = "aliceblue", lwd = 0.05) +
+  geom_sf(data = gadm1_aqli_2022_shp %>% filter(name0 == "Pakistan"), color = "azure4", fill = "transparent", lwd = 0.15) +
+  geom_sf(data = gadm0_aqli_2022_shp %>% filter(name0 == "Pakistan"), color = "cornsilk4", fill = "transparent", lwd = 0.5) +
+  ggthemes::theme_map() + 
+  scale_fill_manual(values = c("0 to < 0.1" = "#ffffff", 
+                               "0.1 to < 0.5" = "#ffeda0", 
+                               "0.5 to < 1" = "#fed976", 
+                               "1 to < 2" = "#feb24c", 
+                               "2 to < 3" = "#fd8d3c", 
+                               "3 to < 4" = "#fc4e2a", 
+                               "4 to < 5" = "#e31a1c", 
+                               "5 to < 6" = "#bd0026", 
+                               ">= 6" = "#800026")) +
+  ggthemes::theme_map() +
+  labs(fill = "Potential gain in life expectancy (Years)") + 
+  theme(legend.position = "bottom", 
+        legend.justification = c(0.5, 3), 
+        legend.background = element_rect(color = "black"), 
+        legend.text = element_text(size = 14),
+        legend.title = element_text(size = 15), 
+        plot.title = element_text(hjust = 0.5, size = 15), 
+        legend.box.margin = margin(b = 1, unit = "cm"),
+        plot.subtitle = element_text(hjust = 0.5, size = 7), 
+        plot.caption = element_text(hjust = 0.7, size = 9, face = "italic"), 
+        legend.key = element_rect(color = "black"), 
+        legend.box.spacing = unit(0, "cm"), 
+        legend.direction = "horizontal") +
+  guides(fill = guide_legend(nrow = 1))
---
+++ july.2024/factsheets/pakistan/fig2/script.R
@@ -0,0 +1,57 @@
+# read in the helper file
+source("R/july.2024.helper.script.R")
+
+# read and filter AQLI data
+pak_aqli_2022 <- gadm2_aqli_2022 %>%
+  filter(country == "Pakistan") %>%
+  mutate(region = case_when(
+    name_1 == "Islamabad" ~ "Islamabad",
+    name_1 == "Punjab" ~ "Punjab",
+    name_1 != "Islamabad"| name_1 != "Punjab" ~ "All other regions (excluding Islamabad and Punjab)"))
+
+# pakistan fs fig 2 data
+pak_fs_fig2_dataset <- pak_aqli_2022 %>%
+  select('country', 'name_1', 'name_2', 'population', 'pm2022', 'llpp_who_2022', 'llpp_nat_2022') %>%
+  group_by(name_1) %>%
+  mutate(pop_weights = population/sum(population, na.rm = TRUE),
+         pm2022_pop_weighted = pop_weights*pm2022,
+         llpp_who_2022_pop_weighted = pop_weights*llpp_who_2022,
+         llpp_nat_2022_pop_weighted = pop_weights*llpp_nat_2022) %>%
+  summarise(tot_pop = sum(population, na.rm = TRUE), 
+            avg_pm2.5_2022 = sum(pm2022_pop_weighted, na.rm = TRUE),
+            le_gain = (avg_pm2.5_2022 - who_guideline)*le_constant,
+            llpp_who_2022 = sum(llpp_who_2022_pop_weighted, na.rm = TRUE),
+            llpp_nat_2022 = sum(llpp_nat_2022_pop_weighted, na.rm = TRUE),
+            le_gain = ifelse(le_gain < 0 , 0, le_gain)) %>%
+  slice_max(tot_pop, n = 10) %>%
+  add_aqli_color_scale_buckets("lyl", "llpp_who_2022")
+
+# pakistan fs figure 2
+pak_fs_fig2 <- pak_fs_fig2_dataset %>%
+  ggplot() +
+  geom_col(mapping = aes(x = reorder(name_1, llpp_who_2022), y = llpp_who_2022, fill = lyl_bucket), width = 0.5) +
+  labs(x = "Province", y = "Potential Gain in Life Expectancy  (Years)", fill = "Potential gain in life expectancy (Years)") +
+  scale_y_continuous(breaks = seq(0, 5, 1), limits = c(0, 5)) +
+  scale_fill_manual(values = c("0 to < 0.1" = "#ffffff", 
+                               "0.1 to < 0.5" = "#ffeda0", 
+                               "0.5 to < 1" = "#fed976", 
+                               "1 to < 2" = "#feb24c", 
+                               "2 to < 3" = "#fd8d3c", 
+                               "3 to < 4" = "#fc4e2a", 
+                               "4 to < 5" = "#e31a1c", 
+                               "5 to < 6" = "#bd0026", 
+                               ">= 6" = "#800026")) +
+  coord_flip() + 
+  ggthemes::theme_tufte() +
+  theme(legend.position = "bottom", 
+        plot.title = element_text(hjust = 0.5, size = 16), 
+        plot.subtitle = element_text(hjust =  0.5, size = 10, face = "italic", margin = margin(b = 0.8, unit = "cm")), 
+        plot.caption = element_text(size = 8, hjust = 0, face = "italic"),
+        legend.box.background = element_rect(color = "black"),
+        plot.background = element_rect(color = "white"),
+        axis.line = element_line(), 
+        axis.text = element_text(size = 11), 
+        axis.title = element_text(size = 13), 
+        axis.title.y = element_text(margin = margin(r = 0.7, unit = "cm")), 
+        axis.title.x = element_text(margin = margin(t = 0.6, b = 0.6, unit = "cm")), 
+        axis.ticks = element_blank()) 
