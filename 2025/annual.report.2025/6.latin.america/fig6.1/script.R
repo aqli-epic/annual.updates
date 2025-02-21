a18aea2527dd0576157f11cecede4f7e77d1fe6f
Hrishikesh Chandra gautam <153756763+hcgautam@users.noreply.github.com>
2025-02-21 16:35:05 +0530

    Updated Latam chapter figures and code

--- 2025/annual.report.2025/6.latin.america/fig6.1/script.R
+++ 2025/annual.report.2025/6.latin.america/fig6.1/script.R
@@ -8,7 +8,7 @@ diseases_list <- c("Tobacco",
                    "Child and maternal malnutrition",
                    "Self-harm and interpersonal violence",
                    "PM2.5 relative to WHO guideline")
-ar_latam_fig6.1_data <- gbd_results_master_2023 %>%
+ar_latam_fig6.1_data <- gbd_results_master_2025 %>%
   filter(cause_of_death %in% diseases_list, 
          country %in% country_list)
 
@@ -35,7 +35,7 @@ ar_latam_fig6.1 <- ar_latam_fig6.1_data %>%
   ggplot(mapping = aes(x = reorder_within(cause_of_death, lyl, country), y = lyl)) +
   geom_col(mapping = aes(fill = cause_of_death), width = 0.5, color = "white") +
   scale_x_reordered() +
-  ylim(0,2.5) +
+  ylim(0,3) +
   facet_wrap(~factor(country, levels = country_list), scales = "free_x", ncol = 5) +
   scale_fill_manual(values = c("#5e92a9", "#7f152c" , "#8ea75b","#f29e37")) +
   labs(x = "Threats to Life Expectancy", y = "Life Years Lost", title = "",
