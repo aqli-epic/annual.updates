175f7297978a4dc060f4b121deb02b1c5968feb3
Nishka Sharma <71686549+nishkasharma@users.noreply.github.com>
2025-04-28 16:38:20 -0700

    US factsheet figures and figure-wise code

---
+++ 2025/factsheets/usa/fig2/script.R
@@ -0,0 +1,44 @@
+# read in the helper file
+source("R/july.2024.helper.script.R")
+
+# Figure 2: Potential gain in life expectancy from permanently reducing PM2.5 ------
+# from 2023 concentration to the WHO guideline
+
+# US figure 2 data
+us_fs_fig2_data <- gadm2_aqli_2023 %>%
+  filter(country == "United States", name_1 %notin% c("Alaska", "Hawaii")) %>%
+  left_join(gadm2_aqli_2023_shp, by = c("objectid_gadm2" = "obidgadm2")) %>%
+  add_aqli_color_scale_buckets("lyl", "llpp_who_2023") %>%
+  select(-geometry, geometry) %>%
+  st_as_sf()
+
+# US figure 2
+us_fs_fig2 <- us_fs_fig2_data %>%
+  ggplot() +
+  geom_sf(mapping = aes(fill = forcats::fct_reorder(lyl_bucket, order_lyl_bucket)), color = "aliceblue", lwd = 0.05) +
+  geom_sf(data = gadm1_aqli_2023_shp %>% filter(name0 == "United States", name1 %notin% c("Alaska", "Hawaii")), color = "azure4", fill = "transparent", lwd = 0.3) +
+  scale_fill_manual(values = c("0 to < 0.1" = "#FFFFFF",
+                               "0.1 to < 0.5" = "#FFF2E1",
+                               "0.5 to < 1" = "#FFEDD3",
+                               "1 to < 2" = "#FFC97A",
+                               "2 to < 3" = "#FFA521",
+                               "3 to < 4" = "#EB6C2A",
+                               "4 to < 5" = "#D63333",
+                               "5 to < 6" = "#8E2946",
+                               ">= 6" = "#451F59")) +
+  ggthemes::theme_map() +
+  labs(fill = "Potetial gain in life expectancy (Years)  ", title = "") +
+  theme(legend.position = "bottom",
+        legend.justification = "center",
+        legend.background = element_rect(color = "black"),
+        legend.text = element_text(size = 20, color="#222222"),
+        legend.title = element_text(size = 20, color="#222222"),
+        plot.title = element_text(hjust = 0.5, size = 15),
+        legend.box.margin = margin(b = 1, unit = "cm"),
+        plot.subtitle = element_text(hjust = 0.5, size = 12),
+        plot.caption = element_text(hjust = 0.7, size = 9, face = "italic"),
+        legend.key = element_rect(color = "black"),
+        legend.box.spacing = unit(0, "cm"),
+        legend.direction = "horizontal",
+        plot.background = element_rect(fill = "white", color = "white")) +
+  guides(fill = guide_legend(nrow = 1))
