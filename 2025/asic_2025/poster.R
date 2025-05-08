a50a1119632e5cc288edf03f3c3784a09f6fe2f9
Nishka Sharma <71686549+nishkasharma@users.noreply.github.com>
2025-05-08 16:38:27 -0700

    ASIC poster plot design

--- 2025/asic_2025/poster.R
+++ 2025/asic_2025/poster.R
@@ -16,7 +16,7 @@ first_year <- 1998
 # colour scale
 source("~/Desktop/AQLI/REPO/annual.updates/R/colour_scale.R")
 
-#### data #### 
+#### data ####
 # read in latest PM2.5 data file
 gadm0_aqli_2022 <- read_csv("~/Desktop/AQLI/2024 AQLI Update/data/gadm0_2022_wide.csv")
 public_data <- read_csv("~/Desktop/AQLI/2024 AQLI Update/website/open_data.csv")
@@ -34,55 +34,61 @@ plot_data <- gadm0_aqli_2022 %>%
   inner_join(opp_score %>% select(country, monitor_density), by = c("name" = "country"))
 
 plot_data <- plot_data %>%
-  mutate(nat_std_qtile = case_when(natstandard <= 15 ~ 1,
-                                   natstandard > 15 & natstandard <= 30 ~ 2, 
-                                   natstandard > 30 ~ 3,
-                                   is.na(natstandard) ~ 4),
+  mutate(nat_std_qtile = case_when(is.na(natstandard) ~ 1,
+                                   natstandard > 30 ~ 2,
+                                   natstandard <= 30 & natstandard > 15 ~ 3,
+                                   natstandard <= 15 ~ 4),
          mon_dens_qtile = case_when(is.na(monitor_density) ~ 1,
                                     monitor_density < 0.1 ~ 2,
-                                    monitor_density >= 0.1 & monitor_density < 1 ~ 3, 
-                                    monitor_density >= 1 & monitor_density < 4 ~ 4, 
+                                    monitor_density >= 0.1 & monitor_density < 1 ~ 3,
+                                    monitor_density >= 1 & monitor_density < 4 ~ 4,
                                     monitor_density >= 4 & monitor_density < 10 ~ 5,
                                     monitor_density >= 10 ~ 6)) %>%
-  replace_na(list(nat_std_qtile = 6, mon_dens_qtile = 0)) %>%
+  # replace_na(list(natstandard = 60, monitor_density = 0)) %>%
   add_aqli_color_scale_buckets("lyl", "who2022")
 
 plot_data %>%
-  ggplot(aes(x = mon_dens_qtile, y = nat_std_qtile, size = lyl_bucket, fill = lyl_bucket)) +
-  geom_jitter(width = 0.5, height = 0.5, alpha = 0.7, shape = 21) +
-  geom_hline(yintercept = 1, linetype = "solid", lwd = 2) +
+  ggplot(aes(x = mon_dens_qtile, y = nat_std_qtile, fill = lyl_bucket)) +
+  # geom_point(alpha = 0.7, shape = 21, size = 10) +
+  geom_jitter(width = 0.5, height = 0.5, alpha = 0.7, shape = 21, size = 10) +
+  # geom_text(aes(label = name), size = 6, fontface = "bold", check_overlap = TRUE) +
+  geom_hline(yintercept = 3, linetype = "solid", lwd = 2) +
   geom_vline(xintercept = 3.1, linetype = "solid", lwd = 2) +
-  # annotate("text", x = -1, y = 5.5, label = "Relaxed Standard\n+ Low Monitoring", hjust = 0) +
-  # annotate("text", x = 7, y = 5.5, label = "Relaxed Standard\n+ High Monitoring", hjust = 1) +
-  # annotate("text", x = -1, y = 2.5, label = "Strict Standard\n+ Low Monitoring", hjust = 0) +
-  # annotate("text", x = 7, y = 2.5, label = "Strict Standard\n+ High Monitoring", hjust = 1) +
-  scale_fill_manual(values = c("0 to < 0.1" = "#FFFFFF", 
-                               "0.1 to < 0.5" = "#FFF2E1", 
-                               "0.5 to < 1" = "#FFEDD3", 
-                               "1 to < 2" = "#FFC97A", 
-                               "2 to < 3" = "#FFA521", 
-                               "3 to < 4" = "#EB6C2A", 
-                               "4 to < 5" = "#D63333", 
-                               "5 to < 6" = "#8E2946", 
+  annotate("text", x = -1, y = 5.5, hjust = 0, size = 6,
+           label = "National Standard < 15 µg/m³, \nMonitoring Density < 1 per million people") +
+  annotate("text", x = 7, y = 5.5, hjust = 1, size = 6,
+           label = "National Standard < 15 µg/m³, \nMonitoring Density > 1 per million people") +
+  annotate("text", x = -1, y = 0.5, hjust = 0, size = 6,
+           label = "National Standard > 15 µg/m³, \nMonitoring Density < 1 per million people") +
+  annotate("text", x = 7, y = 0.5, hjust = 1, size = 6,
+           label = "National Standard > 15 µg/m³, \nMonitoring Density > 1 per million people") +
+  scale_fill_manual(values = c("0 to < 0.1" = "#FFFFFF",
+                               "0.1 to < 0.5" = "#FFF2E1",
+                               "0.5 to < 1" = "#FFEDD3",
+                               "1 to < 2" = "#FFC97A",
+                               "2 to < 3" = "#FFA521",
+                               "3 to < 4" = "#EB6C2A",
+                               "4 to < 5" = "#D63333",
+                               "5 to < 6" = "#8E2946",
                                ">= 6" = "#451F59")) +
-  scale_size_manual(values = c("0 to < 0.1" = 2,
-                               "0.1 to < 0.5" = 4,
-                               "0.5 to < 1" = 6,
-                               "1 to < 2" = 8,
-                               "2 to < 3" = 10,
-                               "3 to < 4" = 12,
-                               "4 to < 5" = 14,
-                               "5 to < 6" = 16,
-                               ">= 6" = 18)) +
-  guides(size = guide_legend(title = "Potential gain in \nlife expectancy (years)"),
+  # scale_size_manual(values = c("0 to < 0.1" = 2,
+  #                              "0.1 to < 0.5" = 4,
+  #                              "0.5 to < 1" = 6,
+  #                              "1 to < 2" = 8,
+  #                              "2 to < 3" = 10,
+  #                              "3 to < 4" = 12,
+  #                              "4 to < 5" = 14,
+  #                              "5 to < 6" = 16,
+  #                              ">= 6" = 18)) +
+  guides(#size = guide_legend(title = "Potential gain in \nlife expectancy (years)"),
          fill = guide_legend(title = "Potential gain in \nlife expectancy (years)")) +
-  labs(x = "Monitor Density (Less Dense to More Dense)", y = "National Standard (Strict to Relaxed)") +
+  labs(x = "Monitor Density", y = "National Standard") +
   theme_tufte() +
   theme(legend.position = "right",
-        legend.text = element_text(size = 18),
-        legend.title = element_text(size = 20),
+        legend.text = element_text(size = 36),
+        legend.title = element_text(size = 40),
         axis.text = element_blank(),
         axis.ticks = element_blank(),
-        axis.title.x = element_text(size = 20, margin = margin(r = 0.3, unit = "cm")),
-        axis.title.y = element_text(size = 20, margin = margin(r = 0.3, unit = "cm")))
+        axis.title.x = element_text(size = 40, margin = margin(r = 0.3, unit = "cm")),
+        axis.title.y = element_text(size = 40, margin = margin(r = 0.3, unit = "cm")))
 
